a3a3576ff18609c9fb1866f554b17e7b47ea9ed1
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-02-25 17:49:25 +0000

    AppCounter, Counter.jsx update - total

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,14 @@
 import React, {useState} from 'react';
 
-export default function Counter() {
+export default function Counter({total, onClick}) {
   const [count, setCount] = useState(0);
   return (    
     <div className='counter'>
-      <span className='number'>{count}</span>
+      <p className='number'>{count}<span className='total'>/{total}</span></p>
       <button className='button' 
         onClick={() => {
           setCount((prev) => prev + 1);
-          setCount((prev) => prev + 1);
-          setCount((prev) => prev + 1);
-          setCount((prev) => prev + 1);
-          setCount((prev) => prev + 1);
+          onClick();
         }}
       >
         Add +
@@ -19,3 +16,4 @@ export default function Counter() {
     </div>
   );
 }
+
