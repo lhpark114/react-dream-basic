c32bd20966019d733d2c617645ba1df0aab9cb29
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-02-25 19:35:27 +0000

    useEffect + dependency additional

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
-  const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
-
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => setChecked((prev) => !prev);
 
   // 무한 루프에 빠지므로 생략해줌 //
   // useEffect(() => {
@@ -21,7 +21,7 @@ export default function Products() {
 
   // 위의 fetch 대신 useEffect사용
     useEffect(() => {
-      fetch('data/products.json')
+      fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then(res => res.json())
       .then(data => {
         console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
@@ -30,10 +30,13 @@ export default function Products() {
       return () => {
         console.log('🧹깨끗하게 청소하는 일들을 합니다.')
       }
-    }, []);
+    }, [checked]);
   
   return (
     <>
+      <input id="checkbox" type='checkbox' value={checked} onChange={handleChange} />
+        <label htmlFor='checkbox'>Show Only 🔥 Sale </label>
+    
       <ul>
         {products.map((product) => (
           <li key={product.id}>
@@ -44,7 +47,7 @@ export default function Products() {
           </li>
         ))}
       </ul>
-      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+{/*       <button onClick={() => setCount((prev) => prev + 1)}>{count}</button> */}
     </>
   );
 }
